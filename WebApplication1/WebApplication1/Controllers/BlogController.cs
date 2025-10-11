e9006fd2176f3189d25d98bbeccb8da5b102ad0a
PrzeSoltys <przemyslaw.soltys@microsoft.wsei.edu.pl>
2025-10-11 18:25:58 +0200

    Zadania
    Wykonałem wszystkie zadania z 04.10.2025

--- WebApplication1/WebApplication1/Controllers/BlogController.cs
+++ WebApplication1/WebApplication1/Controllers/BlogController.cs
@@ -6,17 +6,45 @@ namespace WebApplication1.Controllers
     public class BlogController : Controller
     {
         private readonly ILogger<BlogController> _logger;
+        private static readonly List<BlogArticleViewModel> _articles = new()
+        {
+            new BlogArticleViewModel { Id = "1", Title = "Welcome to my Blog.", Description = "Intro post about the blog.", Content = "Help!"},
+            new BlogArticleViewModel { Id = "2", Title = "Learning ASP.NET MVC.", Description = "Baisics of ASP.NET MCV.", Content = "Help!"},
+            new BlogArticleViewModel { Id = "3", Title = "Understanding Routing.", Description = "How routing works.", Content = "Help!"}
+        };
         public BlogController(ILogger<BlogController> logger)
         {
             _logger = logger;
         }
         public IActionResult Index()
         {
-            return View(new BlogViewModel()
+            return View(_articles);
+        }
+        public IActionResult Article(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id)) return NotFound();
+            var post = _articles.FirstOrDefault(p => p.Id.Equals(id, StringComparison.OrdinalIgnoreCase));
+            if (post == null) return NotFound();
+            return View(post);
+        }
+        [HttpGet]
+            public IActionResult Create()
             {
-                Title = "Welcome to My Blog!",
-                Description = "Tis is a simple blog application built with ASP.MCV."
-            });
+                return View(new CreateBlogArticleModel());
+            }
+        [HttpPost]
+        public IActionResult Create(CreateBlogArticleModel model)
+        {
+            if (string.IsNullOrWhiteSpace(model.Id)) return Content("Provide an ID.");
+            var stored = new BlogArticleViewModel
+            {
+                Id = model.Id,
+                Title = model.Title,
+                Description = model.Description,
+                Content = model.Content
+            };
+            _articles.Add(stored);
+            return RedirectToAction(nameof(Article), new {id = stored.Id });
         }
     }
 }
--- WebApplication1/WebApplication1/Models/BlogArticleViewModel.cs
+++ WebApplication1/WebApplication1/Models/BlogArticleViewModel.cs
@@ -1,8 +1,10 @@
 ﻿namespace WebApplication1.Models
 {
-    public class BlogViewModel
+    public class BlogArticleViewModel
     {
+        public string Id { get; set; }
         public string Title {  get; set; }
         public string Description { get; set; }
+        public string Content {  get; set; }
     }
 }
